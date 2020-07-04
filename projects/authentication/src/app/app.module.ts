3836aeb15e9a7383e3bc5cad7ba976cd94433e87
Michael Jean <mpjean93@gmail.com>
2020-07-04 12:13:03 -0400

    Adds initial set of changes for sign in/up

--- projects/authentication/src/app/app.module.ts
+++ projects/authentication/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { SignUpComponent } from './authentication/sign-up/sign-up.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ThankYouComponent } from './thank-you/thank-you.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-    declarations: [AppComponent, AuthenticationComponent, SignUpComponent, LoginComponent, ThankYouComponent],
+    declarations: [AppComponent, AuthenticationComponent, SignUpComponent, LoginComponent, ThankYouComponent, NavBarComponent],
     imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
