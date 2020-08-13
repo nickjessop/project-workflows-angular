620a5214bfbf70023feae49e8ee2c667398457b0
Michael Jean <mpjean93@gmail.com>
2020-08-13 19:18:47 -0400

    Component cleanup

--- src/app/core/fields/checkbox/checkbox.component.ts
+++ src/app/core/fields/checkbox/checkbox.component.ts
@@ -1,19 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { FieldConfig } from '../../../models/interfaces/core-component';
-import { BaseFieldComponent } from '../base-field/base-field.component';
 
 @Component({
     selector: 'app-checkbox',
     templateUrl: './checkbox.component.html',
     styleUrls: ['./checkbox.component.scss'],
 })
-export class CheckboxComponent extends BaseFieldComponent implements OnInit {
+export class CheckboxComponent implements OnInit {
     @Input() checkboxLabel = 'Option';
     @Input() disabled = false;
 
     constructor() {
-        super();
+        // super();
     }
 
     ngOnInit() {}
--- src/app/core/fields/large-text-input/large-text-input.component.ts
+++ src/app/core/fields/large-text-input/large-text-input.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { FieldConfig } from '../../../models/interfaces/core-component';
+import { Component, OnInit, Input } from '@angular/core';
+import { FieldConfig, ComponentMode } from 'src/app/models/interfaces/core-component';
 import { FormGroup } from '@angular/forms';
-import { BaseFieldComponent } from '../base-field/base-field.component';
 
 @Component({
     selector: 'app-large-text-input',
     templateUrl: './large-text-input.component.html',
     styleUrls: ['./large-text-input.component.scss'],
 })
-export class LargeTextInputComponent extends BaseFieldComponent implements OnInit {
-    constructor() {
-        super();
-    }
+export class LargeTextInputComponent implements OnInit {
+    @Input() field!: FieldConfig;
+    @Input() group!: FormGroup;
+    @Input() componentMode: ComponentMode = 'view';
+
+    constructor() {}
 
     ngOnInit() {}
 }
