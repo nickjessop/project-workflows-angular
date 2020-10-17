54800981df2c0d364475d82a2411c4251b9b337d
Nick Jessop <nick@parachutelabs.ca>
2020-10-17 14:46:26 -0400

    Adds prime text field to small text block

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -4,6 +4,7 @@ import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
 import { GalleriaModule } from 'primeng/galleria';
 import { InplaceModule } from 'primeng/inplace';
+import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { TableModule } from 'primeng/table';
 
@@ -17,7 +18,16 @@ import { TableModule } from 'primeng/table';
         GalleriaModule,
         TableModule,
         InplaceModule,
+        InputTextModule,
     ],
-    exports: [AccordionModule, ButtonModule, InputTextareaModule, GalleriaModule, TableModule, InplaceModule],
+    exports: [
+        AccordionModule,
+        ButtonModule,
+        InputTextareaModule,
+        GalleriaModule,
+        TableModule,
+        InplaceModule,
+        InputTextModule,
+    ],
 })
 export class PrimengModule {}
