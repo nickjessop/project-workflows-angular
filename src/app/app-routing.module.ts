ec171c30951069299972923a8e44f52b4a2cbeb5
Michael Jean <mpjean93@gmail.com>
2021-01-03 16:02:42 -0500

    redirect to dashboard if logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProtectedGuard } from './protected.guard';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'auth/login', component: AuthenticationComponent },
-    { path: 'auth/register', component: AuthenticationComponent },
+    { path: 'auth/login', component: AuthenticationComponent, canActivate: [ProtectedGuard] },
+    { path: 'auth/register', component: AuthenticationComponent, canActivate: [ProtectedGuard] },
     { path: '404', component: PageNotFoundComponent },
     { path: '**', component: PageNotFoundComponent },
 ];
