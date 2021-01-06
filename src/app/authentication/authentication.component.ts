df6fd1470703bea51a52bcc5d29f5037300340af
Nick Jessop <nick@parachutelabs.ca>
2021-01-05 20:57:08 -0500

    Adds register page with URL parmeters

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -7,12 +7,30 @@ import { AuthenticationService } from '../services/authentication/authentication
     styleUrls: ['./authentication.component.scss'],
 })
 export class AuthenticationComponent implements OnInit {
-    public authMode: 'register' | 'login' = 'login';
+    // public authMode: 'register' | 'login' = 'login';
+    public authMode: 'register' | 'login' = 'register';
     public authInfo = { email: '', password: '' };
+
+    public email: string | null = '';
+    public plan: string | null = 'Essential';
+    public planPrice: string | null = '$9';
 
     constructor(private authService: AuthenticationService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        const queryString = window.location.search;
+        if (queryString) {
+            const urlParams = new URLSearchParams(queryString);
+            this.email = urlParams.get('email');
+            this.plan = urlParams.get('plan');
+            this.planPrice = urlParams.get('planPrice');
+        }
+    }
+
+    public register() {
+        // this.authService.register()
+        console.log('sign up');
+    }
 
     public login() {
         const email = this.authInfo.email;
