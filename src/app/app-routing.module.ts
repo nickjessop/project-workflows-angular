bc1e24917eb72c3a5a7dc6f2be2bfaf48c09b8f4
Michael Jean <mpjean93@gmail.com>
2021-01-08 17:51:39 -0500

    Add override to only auth screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'auth/login', component: AuthenticationComponent },
     { path: 'auth/register', component: AuthenticationComponent },
-    { path: '404', component: PageNotFoundComponent },
-    { path: '**', component: PageNotFoundComponent },
+    // { path: '404', component: PageNotFoundComponent },
+    { path: '**', redirectTo: 'auth/login' },
 ];
 
 @NgModule({
