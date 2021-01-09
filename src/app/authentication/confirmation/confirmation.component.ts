7de011adcb8939b595aa12aa1b799d38eab07671
Nick Jessop <nick@parachutelabs.ca>
2021-01-09 11:35:23 -0500

    Fixes Twitter share button

--- src/app/authentication/confirmation/confirmation.component.ts
+++ src/app/authentication/confirmation/confirmation.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-confirmation',
-  templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.scss']
+    selector: 'app-confirmation',
+    templateUrl: './confirmation.component.html',
+    styleUrls: ['./confirmation.component.scss'],
 })
 export class ConfirmationComponent implements OnInit {
+    public msg: string =
+        'I just joined the waitlist for Stepflow, an upcoming app that lets you collaborate more efficiently with people outside your organization. Read more at';
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
