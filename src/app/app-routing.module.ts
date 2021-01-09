1d968657dace96f944f94fa5045ff3f5ef742961
Michael Jean <mpjean93@gmail.com>
2021-01-09 14:22:56 -0500

    Re-enable project dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { ConfirmationComponent } from './authentication/confirmation/confirmation.component';
-// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-    // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'auth/login', component: AuthenticationComponent, data: { authMode: 'login' } },
     { path: 'auth/register', component: AuthenticationComponent, data: { authMode: 'register' } },
     { path: 'auth/confirmation', component: ConfirmationComponent },
-    // { path: '404', component: PageNotFoundComponent },
+    { path: '404', component: PageNotFoundComponent },
     { path: '**', redirectTo: 'auth/login' },
 ];
 
