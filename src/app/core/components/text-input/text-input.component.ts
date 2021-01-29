9cc939234435cc71a98263cfc2d5ada51f63f1d9
Nick Jessop <nick.jessop@icloud.com>
2021-01-28 22:37:24 -0500

    Update src/app/core/components/text-input/text-input.component.ts
    Co-authored-by: mpjean <65505259+mpjean@users.noreply.github.com>

--- src/app/core/components/text-input/text-input.component.ts
+++ src/app/core/components/text-input/text-input.component.ts
@@ -29,7 +29,6 @@ export class TextInputComponent extends BaseFieldComponent implements OnInit {
 
     onFocusOut(event: { srcElement: { clientHeight: string } }) {
         const height = +event.srcElement.clientHeight + 10; //adds small buffer for view mode to avoid scrollbar
-        this.settings = { TextInputComponent: { textareaHeight: height } };
-        this.textInputData.settings = this.settings;
+          this.textInputData.settings = { TextInputComponent: { textareaHeight: height } };
     }
 }
