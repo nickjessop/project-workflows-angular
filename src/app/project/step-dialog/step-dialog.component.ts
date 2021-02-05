ad627e95672416e1dc1588dea458c2a699089999
Nick Jessop <nick@parachutelabs.ca>
2021-02-05 15:52:44 -0500

    Fixes status bug, adds default status, ensures status icon displays for selected item in dropdown

--- src/app/project/step-dialog/step-dialog.component.ts
+++ src/app/project/step-dialog/step-dialog.component.ts
@@ -7,16 +7,22 @@ import { Status, Step } from 'src/app/models/interfaces/project';
     styleUrls: ['./step-dialog.component.scss'],
 })
 export class StepDialogComponent implements OnInit {
-    @Input() step: Step = { title: '', description: '' };
+    @Input() step: Step = {
+        title: '',
+        description: '',
+        status: { label: 'No status', value: 'no-status', icon: 'pi-circle-off' },
+    };
     @Input() mode: 'edit' | 'new' | 'delete' = 'new';
     @Input() showDialog = false;
     @Output() dialogSubmitEvent = new EventEmitter<{ step?: Step; mode: 'edit' | 'new' | 'delete' }>();
     @Output() onHideEvent = new EventEmitter<true>();
 
     statusOptions: Status[];
+    selectedStatus: Status = { label: 'No status', value: 'no-status', icon: 'pi-circle-off' };
 
     constructor() {
         this.statusOptions = [
+            { label: 'No status', value: 'no-status', icon: 'pi-circle-off' },
             { label: 'In progress', value: 'in-progress', icon: 'pi-progress' },
             { label: 'Important', value: 'important', icon: 'pi-exclamation-circle' },
             { label: 'Upcoming', value: 'upcoming', icon: 'pi-clock' },
@@ -24,7 +30,11 @@ export class StepDialogComponent implements OnInit {
         ];
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        if (this.mode === 'edit' && this.step.status) {
+            this.selectedStatus = this.step.status;
+        }
+    }
 
     public getButtonLabel() {
         const componentMode = this.mode;
@@ -43,6 +53,7 @@ export class StepDialogComponent implements OnInit {
         if (this.mode !== 'delete' && (!this.step.title || !this.step.description)) {
             return;
         }
+        this.step.status = this.selectedStatus;
 
         this.dialogSubmitEvent.emit({ step: this.step, mode: this.mode });
     }
