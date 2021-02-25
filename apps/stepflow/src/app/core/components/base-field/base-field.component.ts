9950677b082bfb8cde0b5d20e05b2dfde35a878b
Michael Jean <mpjean93@gmail.com>
2021-02-24 22:07:19 -0500

    migration to nx

---
+++ apps/stepflow/src/app/core/components/base-field/base-field.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { ProjectService } from '../../../services/project/project.service';
+import { BlockConfig, ComponentMode, createBlockConfig } from '../../interfaces/core-component';
+
+@Component({
+    selector: 'app-base-field',
+    templateUrl: './base-field.component.html',
+    styleUrls: ['./base-field.component.scss'],
+})
+export class BaseFieldComponent {
+    @Input() field: BlockConfig = createBlockConfig('textInput');
+    @Input() index?: number;
+    @Input() componentMode: ComponentMode = 'edit';
+    @Input() height?: number;
+
+    public items: MenuItem[] = [
+        {
+            label: 'Delete Block',
+            icon: 'pi pi-times',
+            command: () => {
+                this.onDeleteBlock();
+            },
+        },
+    ];
+
+    constructor(public projectService: ProjectService) {}
+
+    ngOnInit() {}
+
+    public onDeleteBlock() {
+        const index = this.index ? this.index : 0;
+        this.projectService.deleteProjectBlock(index);
+    }
+}
