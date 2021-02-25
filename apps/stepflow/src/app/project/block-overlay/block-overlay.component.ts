3b8764d60d5cc6a70d4aed86827482d02eed8100
Michael Jean <mpjean93@gmail.com>
2021-02-24 22:07:19 -0500

    migration to nx

---
+++ apps/stepflow/src/app/project/block-overlay/block-overlay.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { OverlayPanel } from 'primeng/overlaypanel';
+import { ComponentType } from '../../core/interfaces/core-component';
+@Component({
+    selector: 'project-block-overlay',
+    templateUrl: './block-overlay.component.html',
+    styleUrls: ['./block-overlay.component.scss'],
+})
+export class BlockPanelOverlayComponent implements OnInit {
+    @ViewChild(OverlayPanel) overlayPanel?: OverlayPanel;
+
+    @Output() onSelectNewBlock = new EventEmitter<ComponentType>();
+
+    public blockOptions = this.getBlockOptions();
+
+    displayBlockSidebar = false;
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    private getBlockOptions() {
+        const options = [
+            { enabled: true, icon: 'pi-check-square', label: 'Checklist', component: 'checkboxes' },
+            { enabled: true, icon: 'pi-folder', label: 'Files', component: 'fileUploader' },
+            { enabled: true, icon: 'pi-images', label: 'Images', component: 'imageUploader' },
+            { enabled: true, icon: 'pi-small-text', label: 'Text', component: 'textInput' },
+            { enabled: true, icon: 'pi-large-text', label: 'Rich Text', component: 'richTextInput' },
+            { enabled: true, icon: 'pi-table', label: 'Table', component: 'table' },
+            { enabled: true, icon: 'pi-embed', label: 'Embed', component: 'embed' },
+        ];
+
+        return options;
+    }
+
+    public showAddNewBlockDialog(target?: ViewChild) {
+        this.displayBlockSidebar = true;
+    }
+
+    public onAddNewBlockPress(componentType: ComponentType) {
+        this.onSelectNewBlock.emit(componentType);
+        this.displayBlockSidebar = false;
+    }
+}
---
+++ apps/stepflow/src/app/project/step-dialog/step-dialog.component.ts
@@ -0,0 +1,64 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Status, Step } from '../../models/interfaces/project';
+
+@Component({
+    selector: 'project-step-dialog',
+    templateUrl: './step-dialog.component.html',
+    styleUrls: ['./step-dialog.component.scss'],
+})
+export class StepDialogComponent implements OnInit {
+    @Input() step: Step = {
+        title: '',
+        description: '',
+        status: { label: 'No status', value: 'no-status', icon: 'pi-circle-off' },
+    };
+    @Input() mode: 'edit' | 'new' | 'delete' = 'new';
+    @Input() showDialog = false;
+    @Output() dialogSubmitEvent = new EventEmitter<{ step?: Step; mode: 'edit' | 'new' | 'delete' }>();
+    @Output() onHideEvent = new EventEmitter<true>();
+
+    statusOptions: Status[];
+    selectedStatus: Status = { label: 'No status', value: 'no-status', icon: 'pi-circle-off' };
+
+    constructor() {
+        this.statusOptions = [
+            { label: 'No status', value: 'no-status', icon: 'pi-circle-off' },
+            { label: 'In progress', value: 'in-progress', icon: 'pi-progress' },
+            { label: 'Important', value: 'important', icon: 'pi-exclamation-circle' },
+            { label: 'Upcoming', value: 'upcoming', icon: 'pi-clock' },
+            { label: 'Completed', value: 'completed', icon: 'pi-check-circle' },
+        ];
+    }
+
+    ngOnInit(): void {
+        if (this.mode === 'edit' && this.step.status) {
+            this.selectedStatus = this.step.status;
+        }
+    }
+
+    public getButtonLabel() {
+        const componentMode = this.mode;
+        if (componentMode === 'new') {
+            return 'Add';
+        } else if (componentMode === 'edit') {
+            return 'Save';
+        } else if (componentMode === 'delete') {
+            return 'Delete';
+        }
+
+        return '';
+    }
+
+    public onDialogSubmit($event: Event) {
+        if (this.mode !== 'delete' && (!this.step.title || !this.step.description)) {
+            return;
+        }
+        this.step.status = this.selectedStatus;
+
+        this.dialogSubmitEvent.emit({ step: this.step, mode: this.mode });
+    }
+
+    public onHide($event: Event) {
+        this.onHideEvent.emit(true);
+    }
+}
