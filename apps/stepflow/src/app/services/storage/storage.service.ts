b45d3f9942fd564bb5479661529dd433bda1522c
Nick Jessop <nick@parachutelabs.ca>
2021-03-26 08:55:35 -0400

    Uploads project files based on Id and creates folders for profile & project uploads

--- apps/stepflow/src/app/services/storage/storage.service.ts
+++ apps/stepflow/src/app/services/storage/storage.service.ts
@@ -3,17 +3,27 @@ import { from } from 'rxjs';
 import { v4 as uuid } from 'uuid';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { FirebaseService } from '../firebase/firebase.service';
+import { ProjectService } from '../project/project.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class StorageService {
-    constructor(private firebaseService: FirebaseService, private authenticationService: AuthenticationService) {}
+    constructor(
+        private firebaseService: FirebaseService,
+        private authenticationService: AuthenticationService,
+        private projectService: ProjectService
+    ) {}
 
     public uploadFile(file: File) {
         const fileId: string = uuid();
-        const userId = this.authenticationService.user?.id;
-        const storageRef = this.firebaseService.getStorageInstance().ref(`${userId}/${fileId}`);
+        let pathId = this.authenticationService.user?.id;
+        let folder = 'users';
+        if (this.projectService.projectConfig.id != undefined) {
+            pathId = this.projectService.projectConfig.id;
+            folder = 'projects';
+        }
+        const storageRef = this.firebaseService.getStorageInstance().ref(`${folder}/${pathId}/${fileId}`);
 
         return from(storageRef.put(file));
     }
