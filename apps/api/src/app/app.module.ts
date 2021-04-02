1dab57df553b3aac78ccae4994c51c6f003b91b2
Michael Jean <mpjean93@gmail.com>
2021-04-02 15:00:55 -0400

    Fix module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProjectsController } from './controllers/project/projects/projects.controller';
 import { ProjectController } from './controllers/project/project.controller';
 import { UserController } from './controllers/user/user.controller';
 
 @Module({
     imports: [],
-    controllers: [AppController, ProjectsController, ProjectController, UserController],
+    controllers: [AppController, ProjectController, UserController],
     providers: [AppService],
 })
 export class AppModule {}
