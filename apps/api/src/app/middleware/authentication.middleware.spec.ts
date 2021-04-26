d9709e4e2b6299929d5eb17e651d6c592414c8ce
Michael Jean <mpjean93@gmail.com>
2021-04-25 20:12:30 -0400

    Adds FE&BE token validation and interceptor

---
+++ apps/api/src/app/middleware/authentication.middleware.spec.ts
@@ -0,0 +1,5 @@
+describe('AuthenticationMiddleware', () => {
+    it('should be defined', () => {
+        // expect(new AuthenticationMiddleware()).toBeDefined();
+    });
+});
--- apps/stepflow/src/app/services/api/api.service.ts
+++ apps/stepflow/src/app/services/api/api.service.ts
@@ -1,16 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'apps/stepflow/src/environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+import { from } from 'rxjs/internal/observable/from';
+import { switchMap } from 'rxjs/operators';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ApiService {
+export class ApiService implements HttpInterceptor {
     private API_URL = environment.apiUrl;
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private authenticationService: AuthenticationService) {}
+
+    public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return from(this.authenticationService.getCurrentUser()?.getIdToken() || '').pipe(
+            switchMap(token => {
+                if (token.length > 0) {
+                    const reqClone = req.clone({ headers: req.headers.append('Authorization', token) });
+                    return next.handle(reqClone);
+                } else {
+                    return next.handle(req);
+                }
+            })
+        );
+    }
 
     public testApi() {
-        return this.http.get(this.API_URL);
+        return this.http.get(`${this.API_URL}project`);
     }
+
+    public get(path: string) {}
+
+    public post() {}
+
+    public put() {}
+
+    public delete() {}
+
+    public patch() {}
 }
