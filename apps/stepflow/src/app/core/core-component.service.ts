d238cd93d4c656b77a21d1fe394afda135c44d89
Michael Jean <mpjean93@gmail.com>
2021-09-01 07:59:51 -0400

    Fix unused method causing circular dependency

--- apps/stepflow/src/app/core/core-component.service.ts
+++ apps/stepflow/src/app/core/core-component.service.ts
@@ -1,9 +1,7 @@
-import { ComponentFactoryResolver, Injectable, ViewContainerRef } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Injectable } from '@angular/core';
 import {
     BlockConfig,
     Checkboxes,
-    ComponentMode,
     ComponentType,
     Embed,
     FileUploader,
@@ -13,97 +11,12 @@ import {
     TextInput,
     Validator,
 } from '@stepflow/interfaces';
-import { CheckboxesComponent } from './components/checkboxes/checkboxes.component';
-import { EmbedComponent } from './components/embed/embed.component';
-import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
-import { ImageUploaderComponent } from './components/image-uploader/image-uploader.component';
-import { RichTextInputComponent } from './components/rich-text-input/rich-text-input.component';
-import { TableComponent } from './components/table/table.component';
-import { TextInputComponent } from './components/text-input/text-input.component';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CoreComponentService {
-    constructor(
-        private componentFactoryResolver: ComponentFactoryResolver,
-        private viewContainerRef: ViewContainerRef
-    ) {}
-
-    public resolveComponent(
-        componentType: ComponentType,
-        index: number,
-        field: BlockConfig,
-        group: FormGroup,
-        componentMode: ComponentMode
-    ) {
-        if (componentType === 'fileUploader') {
-            const factory = this.componentFactoryResolver.resolveComponentFactory(FileUploaderComponent);
-            const _componentRef = this.viewContainerRef.createComponent<FileUploaderComponent>(factory);
-            _componentRef.instance.field = field;
-            _componentRef.instance.group = group;
-            _componentRef.instance.index = index;
-            _componentRef.instance.componentMode = componentMode;
-
-            return _componentRef;
-        } else if (componentType === 'checkboxes') {
-            const factory = this.componentFactoryResolver.resolveComponentFactory(CheckboxesComponent);
-            const _componentRef = this.viewContainerRef.createComponent<CheckboxesComponent>(factory);
-            _componentRef.instance.index = index;
-            // _componentRef.instance.field = this.field;
-            // _componentRef.instance.group = this.group;
-            _componentRef.instance.componentMode = componentMode;
-
-            return _componentRef;
-        } else if (componentType === 'imageUploader') {
-            const factory = this.componentFactoryResolver.resolveComponentFactory(ImageUploaderComponent);
-            const _componentRef = this.viewContainerRef.createComponent<ImageUploaderComponent>(factory);
-            _componentRef.instance.field = field;
-            _componentRef.instance.group = group;
-            _componentRef.instance.index = index;
-            _componentRef.instance.componentMode = componentMode;
-
-            return _componentRef;
-        } else if (componentType === 'richTextInput') {
-            const factory = this.componentFactoryResolver.resolveComponentFactory(RichTextInputComponent);
-            const _componentRef = this.viewContainerRef.createComponent<RichTextInputComponent>(factory);
-            _componentRef.instance.field = field;
-            _componentRef.instance.group = group;
-            _componentRef.instance.index = index;
-            _componentRef.instance.componentMode = componentMode;
-
-            return _componentRef;
-        } else if (componentType === 'textInput') {
-            const factory = this.componentFactoryResolver.resolveComponentFactory(TextInputComponent);
-            const _componentRef = this.viewContainerRef.createComponent<TextInputComponent>(factory);
-            _componentRef.instance.field = field;
-            _componentRef.instance.group = group;
-            _componentRef.instance.index = index;
-            _componentRef.instance.componentMode = componentMode;
-
-            return _componentRef;
-        } else if (componentType === 'table') {
-            const factory = this.componentFactoryResolver.resolveComponentFactory(TableComponent);
-            const _componentRef = this.viewContainerRef.createComponent<TableComponent>(factory);
-            _componentRef.instance.field = field;
-            _componentRef.instance.group = group;
-            _componentRef.instance.index = index;
-            _componentRef.instance.componentMode = componentMode;
-
-            return _componentRef;
-        } else if (componentType === 'embed') {
-            const factory = this.componentFactoryResolver.resolveComponentFactory(EmbedComponent);
-            const _componentRef = this.viewContainerRef.createComponent<EmbedComponent>(factory);
-            _componentRef.instance.field = field;
-            _componentRef.instance.group = group;
-            _componentRef.instance.index = index;
-            _componentRef.instance.componentMode = componentMode;
-
-            return _componentRef;
-        }
-
-        return undefined;
-    }
+    constructor() {}
 
     public createBlockConfig(type: ComponentType, label?: string, name?: string) {
         const block: BlockConfig = {
