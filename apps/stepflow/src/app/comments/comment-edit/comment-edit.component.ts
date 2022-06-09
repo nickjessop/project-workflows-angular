c433416116709b949b9d714b1f7becdb0a35c7bd
Sebastien Gobeil <seb29111@hotmail.com>
2022-06-08 23:17:04 -0700

    Add comment filter; show author's email (or some other info) instead of the uid.

--- apps/stepflow/src/app/comments/comment-edit/comment-edit.component.ts
+++ apps/stepflow/src/app/comments/comment-edit/comment-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { Comment } from '@stepflow/interfaces';
+import { Comment, CommentDetail } from '@stepflow/interfaces';
 
 @Component({
   selector: 'comment-edit',
@@ -10,28 +10,28 @@ import { Comment } from '@stepflow/interfaces';
 })
 export class CommentEditComponent implements OnInit {  
   
-  @Input() comment?: Comment;
+  @Input() commentDetail?: CommentDetail;
 
   @Output() onCommentEdit = new EventEmitter<Comment>();
   @Output() onCancel = new EventEmitter<void>();
 
   formGroup: FormGroup = new FormGroup({
-    body: new FormControl(this.comment?.body || '', Validators.required),
+    body: new FormControl(this.commentDetail?.comment.body || '', Validators.required),
   });
 
   constructor() { }
 
   ngOnInit(): void {
-    this.formGroup.get('body')?.setValue(this.comment?.body || '');
+    this.formGroup.get('body')?.setValue(this.commentDetail?.comment.body || '');
   }
 
   onSubmitPressed(): void {
     const newCommentBody = this.formGroup.get('body')?.value || '';
 
     // TODO: Validate and throw an error if the comment body is not valid.
-    if (this.comment) {
-      this.comment.body = newCommentBody;
-      this.onCommentEdit.emit(this.comment);
+    if (this.commentDetail) {
+      this.commentDetail.comment.body = newCommentBody;
+      this.onCommentEdit.emit(this.commentDetail.comment);
     } else {
       const newComment: Comment = { body: newCommentBody };
       this.onCommentEdit.emit(newComment);
--- apps/stepflow/src/app/comments/comment-view/comment-view.component.ts
+++ apps/stepflow/src/app/comments/comment-view/comment-view.component.ts
@@ -1,36 +1,31 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Comment } from '@stepflow/interfaces';
-import { CommentsService } from '../../services/comments/comments.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Comment, CommentDetail } from '@stepflow/interfaces';
 
 @Component({
   selector: 'comment-view',
   templateUrl: './comment-view.component.html',
   styleUrls: ['./comment-view.component.scss']
 })
-export class CommentViewComponent implements OnInit {
+export class CommentViewComponent {
 
-  @Input() comment!: Comment;
+  @Input() commentDetail!: CommentDetail;
 
-  @Output() onCommentEdit = new EventEmitter<Comment>();
+  @Output() onCommentEdit = new EventEmitter<CommentDetail>();
   @Output() onCommentResolve = new EventEmitter<Comment>();
   @Output() onCommentDelete = new EventEmitter<Comment>();
 
-  constructor() {}
-
-  ngOnInit(): void {
-
-  }
-
   editPressed(): void {
-    this.onCommentEdit.emit(this.comment);
+    if (!this.commentDetail.isEditable) return;
+    this.onCommentEdit.emit(this.commentDetail);
   }
 
   deletePressed(): void {
-    this.onCommentDelete.emit(this.comment);
+    if (!this.commentDetail.isDeletable) return;
+    this.onCommentDelete.emit(this.commentDetail.comment);
   }
 
   toggleResolvedPressed(): void {
-    this.comment.resolved = !this.comment.resolved;
-    this.onCommentResolve.emit(this.comment);
+    this.commentDetail.comment.resolved = !this.commentDetail.comment.resolved;
+    this.onCommentResolve.emit(this.commentDetail.comment);
   }
 }
