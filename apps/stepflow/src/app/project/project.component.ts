c2e270e338960f86600120ce174047e3f1304f81
Nick Jessop <nick.jessop@icloud.com>
2022-06-09 11:39:14 -0400

    Updates create and delete project dialogs
    Also improves project dashboard styling

--- apps/stepflow/src/app/project/project.component.ts
+++ apps/stepflow/src/app/project/project.component.ts
@@ -30,7 +30,7 @@ export class ProjectComponent implements OnInit {
 
         try {
             const allProjects = await this.projectService.getProjects();
-            this.allProjects = allProjects.map(project => {
+            this.allProjects = allProjects.map((project) => {
                 return {
                     id: project.itemData.id!,
                     description: project.itemData.description,
@@ -39,8 +39,8 @@ export class ProjectComponent implements OnInit {
                     isOwner: project.isOwner,
                 };
             });
-            this.myProjects = this.allProjects.filter(project => project.isOwner === true);
-            this.sharedProjects = this.allProjects.filter(project => project.isOwner === false);
+            this.myProjects = this.allProjects.filter((project) => project.isOwner === true);
+            this.sharedProjects = this.allProjects.filter((project) => project.isOwner === false);
         } catch (error) {
             // TODO: handle error;
         }
@@ -50,13 +50,13 @@ export class ProjectComponent implements OnInit {
 
     public onDeleteProject($event: { id: string }) {
         this.confirmationService.confirm({
-            message: 'Are you sure you want to delete this project?',
+            message: 'Are you sure you want to delete this project? This action cannot be undone.',
             accept: () => {
                 this.projectService.deleteProject($event.id).then(
-                    success => {
+                    (success) => {
                         this.getProjects();
                     },
-                    error => {
+                    (error) => {
                         console.log(`Error deleting project ${$event.id}`);
                     }
                 );
