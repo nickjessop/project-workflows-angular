f41727ed83e82500a512d53a48a292ba4eb1fa57
Nick Jessop <nick.jessop@icloud.com>
2022-06-20 21:35:19 -0400

    Adds better identification of blocks
    - adds colour property to each block
    - moves block metadata to core-component service
    - adds function to get all block metadata
    - displays icon + colour in drag and resize
    - adds tooltip to each block icon
    - modifies some block overlay hover css

--- apps/stepflow/src/app/project/block-overlay/block-overlay.component.ts
+++ apps/stepflow/src/app/project/block-overlay/block-overlay.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { ComponentType } from '@stepflow/interfaces';
+import { CoreComponentService } from '../../core/core-component.service';
+
 @Component({
     selector: 'project-block-overlay',
     templateUrl: './block-overlay.component.html',
@@ -8,76 +10,23 @@ import { ComponentType } from '@stepflow/interfaces';
 export class BlockPanelOverlayComponent implements OnInit {
     @Output() onSelectNewBlock = new EventEmitter<ComponentType>();
 
-    public blockOptions = this.getBlockOptions();
+    public blockOptions: Array<any> = [];
 
     public displayBlockSidebar = false;
 
-    constructor() {}
+    constructor(private coreComponentService: CoreComponentService) {}
 
-    ngOnInit(): void {}
-
-    private getBlockOptions() {
-        const options = [
-            {
-                enabled: true,
-                icon: 'pi-check-square',
-                label: 'Checklist',
-                description: 'Track a list of items.',
-                component: 'checkboxes',
-            },
-            {
-                enabled: true,
-                icon: 'pi-folder',
-                label: 'Files',
-                description: 'Upload files for users to browse and download.',
-                component: 'fileUploader',
-            },
-            {
-                enabled: true,
-                icon: 'pi-images',
-                label: 'Images',
-                description: 'Upload images and display them in a gallery.',
-                component: 'imageUploader',
-            },
-            {
-                enabled: true,
-                icon: 'pi-large-text',
-                label: 'Text',
-                description: 'Capture notes and ideas with simple editing tools.',
-                component: 'richTextInput',
-            },
-            {
-                enabled: true,
-                icon: 'pi-table',
-                label: 'Table',
-                description: 'Organize data inside a simple table.',
-                component: 'table',
-            },
-            {
-                enabled: true,
-                icon: 'pi-embed',
-                label: 'Embed',
-                description: 'Embed third-party apps directly into a step.',
-                component: 'embed',
-            },
-            {
-                enabled: true,
-                icon: 'pi-file-pdf',
-                label: 'PDF',
-                description: 'Display a PDF with convenient controls.',
-                component: 'pdf',
-            },
-            {
-                enabled: true,
-                icon: 'pi-pencil',
-                label: 'Whiteboard',
-                description: 'Sketch out designs, diagrams, or anything else!',
-                component: 'draw',
-            },
-        ];
-
-        return options;
+    ngOnInit(): void {
+        this.blockOptions = this.coreComponentService.getBlockMetaData();
     }
+
+    public get getPublicBlocks() {
+        return this.blockOptions.filter(block => block.beta === false);
+    }
+
+    public get getBetaBlocks() {
+        return this.blockOptions.filter(block => block.beta === true);
+    } //TODO: Show these to certain users.
 
     public showAddNewBlockDialog(target?: ViewChild) {
         this.displayBlockSidebar = true;
