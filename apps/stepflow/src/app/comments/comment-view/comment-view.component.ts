0db88deef788db73183f8788ef3dab82c549eafb
Nick Jessop <nick.jessop@icloud.com>
2022-10-20 21:45:41 -0400

    Adds profile image

--- apps/stepflow/src/app/comments/comment-view/comment-view.component.ts
+++ apps/stepflow/src/app/comments/comment-view/comment-view.component.ts
@@ -1,31 +1,57 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Comment, CommentDetail } from '@stepflow/interfaces';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'comment-view',
-  templateUrl: './comment-view.component.html',
-  styleUrls: ['./comment-view.component.scss']
+    selector: 'comment-view',
+    templateUrl: './comment-view.component.html',
+    styleUrls: ['./comment-view.component.scss'],
 })
 export class CommentViewComponent {
+    @Input() commentDetail!: CommentDetail;
 
-  @Input() commentDetail!: CommentDetail;
+    @Output() onCommentEdit = new EventEmitter<CommentDetail>();
+    @Output() onCommentResolve = new EventEmitter<Comment>();
+    @Output() onCommentDelete = new EventEmitter<Comment>();
 
-  @Output() onCommentEdit = new EventEmitter<CommentDetail>();
-  @Output() onCommentResolve = new EventEmitter<Comment>();
-  @Output() onCommentDelete = new EventEmitter<Comment>();
+    public resolutionLabel = '';
 
-  editPressed(): void {
-    if (!this.commentDetail.isEditable) return;
-    this.onCommentEdit.emit(this.commentDetail);
-  }
+    public commentItems: MenuItem[] = [
+        {
+            label: 'Resolve Comment',
+            icon: 'pi pi-check',
+            command: () => {
+                this.toggleResolvedPressed();
+            },
+        },
+        {
+            label: 'Edit Comment',
+            icon: 'pi pi-pencil',
+            command: () => {
+                this.editPressed();
+            },
+        },
+        {
+            label: 'Delete Comment',
+            icon: 'pi pi-trash',
+            command: () => {
+                this.deletePressed();
+            },
+        },
+    ];
 
-  deletePressed(): void {
-    if (!this.commentDetail.isDeletable) return;
-    this.onCommentDelete.emit(this.commentDetail.comment);
-  }
+    editPressed(): void {
+        if (!this.commentDetail.isEditable) return;
+        this.onCommentEdit.emit(this.commentDetail);
+    }
 
-  toggleResolvedPressed(): void {
-    this.commentDetail.comment.resolved = !this.commentDetail.comment.resolved;
-    this.onCommentResolve.emit(this.commentDetail.comment);
-  }
+    deletePressed(): void {
+        if (!this.commentDetail.isDeletable) return;
+        this.onCommentDelete.emit(this.commentDetail.comment);
+    }
+
+    toggleResolvedPressed(): void {
+        this.commentDetail.comment.resolved = !this.commentDetail.comment.resolved;
+        this.onCommentResolve.emit(this.commentDetail.comment);
+    }
 }
