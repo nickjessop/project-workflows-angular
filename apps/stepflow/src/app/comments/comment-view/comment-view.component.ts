6707affe8357e748f136cd232a5e44107a6c3623
Nick Jessop <nick.jessop@icloud.com>
2022-10-24 13:22:14 -0400

    Adds comment posted at date with formatting

--- apps/stepflow/src/app/comments/comment-view/comment-view.component.ts
+++ apps/stepflow/src/app/comments/comment-view/comment-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Comment, CommentDetail } from '@stepflow/interfaces';
 import { MenuItem } from 'primeng/api';
 
@@ -7,14 +7,15 @@ import { MenuItem } from 'primeng/api';
     templateUrl: './comment-view.component.html',
     styleUrls: ['./comment-view.component.scss'],
 })
-export class CommentViewComponent {
+export class CommentViewComponent implements OnInit {
     @Input() commentDetail!: CommentDetail;
 
     @Output() onCommentEdit = new EventEmitter<CommentDetail>();
     @Output() onCommentResolve = new EventEmitter<Comment>();
     @Output() onCommentDelete = new EventEmitter<Comment>();
 
-    public resolutionLabel = '';
+    public postedAtRelative!: string;
+    public postedAtFormatted!: string;
 
     public commentItems: MenuItem[] = [
         {
@@ -33,6 +34,15 @@ export class CommentViewComponent {
         },
     ];
 
+    ngOnInit() {
+        const currentDate = new Date().getTime();
+        if (this.commentDetail.comment.createdAt) {
+            const postedDate = this.commentDetail.comment.createdAt;
+            this.postedAtRelative = this.relativeTimestamp(currentDate, postedDate);
+            this.postedAtFormatted = this.formatDate(new Date(postedDate));
+        }
+    }
+
     editPressed(): void {
         if (!this.commentDetail.isEditable) return;
         this.onCommentEdit.emit(this.commentDetail);
@@ -46,5 +56,36 @@ export class CommentViewComponent {
     toggleResolvedPressed(): void {
         this.commentDetail.comment.resolved = !this.commentDetail.comment.resolved;
         this.onCommentResolve.emit(this.commentDetail.comment);
+    }
+    relativeTimestamp(current: number, previous: number) {
+        const msPerMinute = 60 * 1000;
+        const msPerHour = msPerMinute * 60;
+        const msPerDay = msPerHour * 24;
+        const msPerMonth = msPerDay * 30;
+        const msPerYear = msPerDay * 365;
+
+        const elapsed = current - previous;
+
+        if (elapsed < msPerMinute) {
+            return Math.round(elapsed / 1000) + ' seconds ago';
+        } else if (elapsed < msPerHour) {
+            return Math.round(elapsed / msPerMinute) + ' minutes ago';
+        } else if (elapsed < msPerDay) {
+            return Math.round(elapsed / msPerHour) + ' hours ago';
+        } else if (elapsed < msPerMonth) {
+            return Math.round(elapsed / msPerDay) + ' days ago';
+        } else if (elapsed < msPerYear) {
+            return Math.round(elapsed / msPerMonth) + ' months ago';
+        } else {
+            return Math.round(elapsed / msPerYear) + ' years ago';
+        }
+    }
+    formatDate(date: Date) {
+        const dd = String(date.getDate()).padStart(2, '0');
+        const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = date.getFullYear();
+        const time = date.toLocaleTimeString('en-US');
+
+        return mm + '/' + dd + '/' + yyyy + ' at ' + time;
     }
 }
