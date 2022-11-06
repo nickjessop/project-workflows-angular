55690fbdb327026e7b421d3dd7e2b6f302608ba7
Michael Jean <mpjean93@gmail.com>
2022-11-06 08:54:09 -0500

    api clean up

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor() {}
 
-    @Get()
+    @Get('healthz')
     getData() {
-        return 'hi';
+        return { status: 'ok' };
     }
 }
