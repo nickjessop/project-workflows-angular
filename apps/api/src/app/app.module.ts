a90ddf3ee8137b8603d388a2b3eb0048254f15a7
Micheal Jean <53185857+mpjean93@users.noreply.github.com>
2022-11-10 20:09:20 -0500

    Chore/clean up api project (#81)
    *Clean up api and stepflow project nx configs

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProjectController } from './controllers/project/project.controller';
 import { UserController } from './controllers/user/user.controller';
 import { AuthenticationMiddleware } from './middleware/authentication.middleware';
@@ -9,7 +8,7 @@ import { FirebaseService } from './services/firebase/firebase.service';
 @Module({
     imports: [],
     controllers: [AppController, ProjectController, UserController],
-    providers: [AppService, FirebaseService],
+    providers: [FirebaseService],
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
